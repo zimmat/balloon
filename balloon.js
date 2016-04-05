5fbc92cf4193fffa9b179789d3b7c4690462cc6f
zimmat <zimkhitha@projectcodex.co>
2016-04-05 17:02:32 +0200

    add

--- balloon.js
+++ balloon.js
@@ -4,41 +4,49 @@ if (orders[orders.length - 1] === ".")
 var oorder = orders.replace('and', '');
 
 var ooder = oorder.trim();
-console.log(ooder);
+// console.log(ooder);
 
 var Oneorder = ooder.split(',');
-console.log(Oneorder);
+// console.log(Oneorder);
 
 var eachOrder = [];
 Oneorder.forEach(function(orders) {
   eachOrder.push(orders.split("sets of"));
 });
-console.log(eachOrder);
-console.log(eachOrder);
 
-// number of ballons ordered
-var numberofOrders = [];
-var color = "";
+    var numberofOrders = {};
 eachOrder.forEach(function(orders) {
       var quantity = Number(orders[0]);
       var color = orders[1];
-      var  orderPerSet = (quantity*3);
-      numberofOrders.push(orderPerSet) ;
-      console.log(numberofOrders);
-
+              numberofOrders[color] = (quantity) * 3;
     });
+console.log(numberofOrders);
 
   // calculating total cost price per set ballon
-  var pricePerBaloon = "R4 for red, R5 for blue, and R5.50 for yellow.";
-  if (pricePerBaloon[pricePerBaloon.length - 1] === ".")
-    pricePerBaloon = pricePerBaloon.slice(0, -1);
-  var pricePerBaloon = pricePerBaloon.replace('and', '');
 
+  var pricePerBaloon = "R4 for red, R5 for blue, and R5.50 for yellow.";
+    if (pricePerBaloon[pricePerBaloon.length - 1] === ".")
+      pricePerBaloon = pricePerBaloon.slice(0, -1);
+    var pricePerBaloon = pricePerBaloon.replace(/R/g,"")
+    .replace(/and /, "");
   var price = pricePerBaloon.split(',');
 
-   price.forEach(function(pricePerBaloon){
-     var priceForEach = pricePerBaloon.split("for");
-     var eachPrice = pricePerBaloon.split("and");
-     console.log(priceForEach);
-   });
-   
+var priceForEach = [];
+     price.forEach(function(pricePerBaloon){
+      priceForEach.push(pricePerBaloon.split("for"));
+     });
+        // console.log(priceForEach);
+
+     var priceOfBaloons = {};
+       priceForEach.forEach(function(baloon){
+           var priceOfOne = Number(baloon[0]);
+           var colorOne = baloon[1];
+           priceOfBaloons[colorOne] = priceOfOne;
+
+         });
+         console.log(priceOfBaloons);
+ // console.log(Object.keys(numberofOrders));
+        //  var totalPrice = Number(Object.keys(numberofOrders) * Object.keys(priceOfBaloons));
+        //  console.log(Object.keys(numberofOrders));
+        //  console.log(Object.keys(priceOfBaloons));
+        //  console.log(totalPrice);
