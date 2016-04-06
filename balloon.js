922a23e5383e6a9a9db4b57cac3ea4253771b4b4
zimmat <zimkhitha@projectcodex.co>
2016-04-06 12:00:35 +0200

    add

--- balloon.js
+++ balloon.js
@@ -44,9 +44,29 @@ var priceForEach = [];
            priceOfBaloons[colorOne] = priceOfOne;
 
          });
-         console.log(priceOfBaloons);
- // console.log(Object.keys(numberofOrders));
-        //  var totalPrice = Number(Object.keys(numberofOrders) * Object.keys(priceOfBaloons));
-        //  console.log(Object.keys(numberofOrders));
-        //  console.log(Object.keys(priceOfBaloons));
-        //  console.log(totalPrice);
+var total = 0;
+
+var color = "";
+  for(keys in numberofOrders){
+    var sum = numberofOrders[keys] * priceOfBaloons[keys];
+    total += sum;
+  }
+ console.log(total);
+
+
+var inflate = 0;
+ for(keys in numberofOrders){
+    var newCost = numberofOrders[keys] * (priceOfBaloons[keys]+2);
+   inflate += newCost;
+ }
+ console.log(inflate);
+
+var people = 25;
+var baloons = 0;
+ for(baloon in numberofOrders){
+   var baloonTotal = (numberofOrders[keys]);
+   baloons += baloonTotal;
+    var sparebaloons = baloons - people;
+ }
+console.log(baloons);
+console.log(sparebaloons);
